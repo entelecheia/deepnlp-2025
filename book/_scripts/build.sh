e1fb5cbf7e08e196de2b736d8d558c7bbd7e8a74
Young Joon Lee <entelecheia@hotmail.com>
2025-11-11 08:42:14 +0900

    feat: update jupyter-book to version 2.0.2 and adjust build scripts for site generation; remove outdated configuration files for English and Korean versions, replacing them with new myst.yml files for improved structure and compatibility.

--- book/_scripts/build.sh
+++ book/_scripts/build.sh
@@ -8,13 +8,17 @@ if [ "$1" == "--all" ]; then
   echo "Building the books with all outputs"
   for lang in en ko; do
     echo "Building the book for $lang"
-    jupyter-book build "book/$lang" --all
+    cd "book/$lang" || exit
+    jupyter-book build --site --all
+    cd ../.. || exit
   done
 else
   echo "Building the books"
   for lang in en ko; do
     echo "Building the book for $lang"
-    jupyter-book build "book/$lang"
+    cd "book/$lang" || exit
+    jupyter-book build --site
+    cd ../.. || exit
   done
 fi
 
